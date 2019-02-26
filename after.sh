1da83e6cf13919fc030805b63f2059bcbe16ac2b
Younes Khoubza <younes.khoubza@gmail.com>
2019-02-26 17:30:24 +0100

    update .gitignore file

---
+++ after.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# If you would like to do some extra provisioning you may
+# add any commands you wish to this file and they will
+# be run after the Homestead machine is provisioned.
+#
+# If you have user-specific configurations you would like
+# to apply, you may also create user-customizations.sh,
+# which will be run after this script.
+
+#export DEBIAN_FRONTEND=noninteractive
+
+#php72
+#
+#sudo apt-get -y \
+#    -o Dpkg::Options::="--force-confdef" \
+#    -o Dpkg::Options::="--force-confold" \
+#    update
+#
+#sudo apt-get -y \
+#    -o Dpkg::Options::="--force-confdef" \
+#    -o Dpkg::Options::="--force-confold" \
+#    install php-bz2 php-apcu php-imagick
